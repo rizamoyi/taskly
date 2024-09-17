f2d55024b7f4203bbd4aafdac8b94aad9b7c2373
rizamoyi <samwelmoyi@gmail.com>
2024-09-17 21:39:52 +0300

    tab navigation using expo router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,40 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { theme } from "../theme";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Shopping list" }} />
-    </Stack>
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping list",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="clockcircleo" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="lightbulb" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
---
+++ app/counter/_layout.tsx
@@ -0,0 +1,30 @@
+import { Stack, Link } from "expo-router";
+import { Pressable } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { theme } from "../../theme";
+
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "counter",
+          headerRight: () => {
+            return (
+              <Link href="/counter/history" asChild>
+                <Pressable hitSlop={20}>
+                  <MaterialIcons
+                    name="history"
+                    size={32}
+                    color={theme.colorGrey}
+                  />
+                </Pressable>
+              </Link>
+            );
+          },
+        }}
+      />
+    </Stack>
+  );
+}
